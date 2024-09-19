68082552b638b68dcab16334c6557c745b997d6b
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-09-19 18:25:22 +0300

    Corrected the layout

--- src/Pages/WorksPage/WorksPage.tsx
+++ src/Pages/WorksPage/WorksPage.tsx
@@ -1,23 +1,27 @@
-import React, { Fragment, useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../store/reduxHooks'
-import { CardWorks } from '../../UI_Components'
-import { Link } from 'react-router-dom'
-import { FETCH_WORKS } from '../../store/slice'
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/reduxHooks";
+import { CardWorks } from "../../UI_Components";
+import { Link } from "react-router-dom";
+import { FETCH_WORKS } from "../../store/slice";
 
 const WorksPage = () => {
-    const works = useAppSelector(state => state.page.data.works)
-    const dispatch = useAppDispatch()
+    const works = useAppSelector((state) => state.page.data.works);
+    const dispatch = useAppDispatch();
     useEffect(() => {
-      dispatch(FETCH_WORKS())
-    }, [dispatch])
-  return (
-    <Fragment>
-     {works.map((work) => {
-        const key = Math.random().toString(30).substring(2, 15)
-        return <Link to={`/work/${work._id}`} key={key} ><CardWorks {...work} /></Link>
-     })}
-    </Fragment>
-  )
-}
+        dispatch(FETCH_WORKS());
+    }, [dispatch]);
+    return (
+        <div style={{ margin: "30px" }}>
+            {works.map((work) => {
+                const key = Math.random().toString(30).substring(2, 15);
+                return (
+                    <Link to={`/work/${work._id}`} key={key}>
+                        <CardWorks {...work} />
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
 
-export default WorksPage
+export default WorksPage;
